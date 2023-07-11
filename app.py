4fe7ff21735bfa88599d78da17ff66d76477a64e
hadi-mu <hadimuhammad100@outlook.com>
2023-07-11 22:59:31 +0100

    Created a skeleton for the search function.

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ sourcesToUse=[]
 authorsToUse=[]
 typesToUse=[]
 sumText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-prevHeadings=["Provisional Report 2022","Car Accidents 2019","Fishing Accidents 2021"]
-prevText=[" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum comes from a line in section 1.10.32."]*3
+prevHeadings=["Another","Provisional Report 2022","Car Accidents 2019","Fishing Accidents 2021"]
+prevText=["Test text ignore me"]*4
 app=Flask(__name__)
 app.static_folder='./static'
 
@@ -30,7 +30,12 @@ def changeDate():
          return render_template('main.html', sources=SOURCES, authors=AUTHORS,types=TYPES,summary=sumText,headings=prevHeadings,previewText=prevText
           )
         
+def searchQuery(query,startDate=None,endDate=None,sources=None,authors=None,types=None):
+        
+        #TO IMPLEMENT
 
+
+        return summary, articleHeadings, articleSummaries, articleLinks
 
 '''''''''
 @app.route("/search_genappbuilder", methods=["POST"])
